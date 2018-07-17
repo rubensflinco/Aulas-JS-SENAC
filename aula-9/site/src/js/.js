655c0bb5b631f7ef26da286d9896211d3b553dde
Rubens Flinco <planetacraftvip@hotmail.com.br>
2018-07-17 08:19:27 -0300

    Aula 9 - ajustes

--- aula-9/site/src/js/.js
+++
@@ -1,5 +0,0 @@
-// mudar a cor do quadrado de tempo em tempo
-setInterval(function () {
-    var random = Math.floor(Math.random() * 100) + 900;
-    $(".quadrado").animate({backgroundColor: "#"+random}, 3000);
-}, 5000);
